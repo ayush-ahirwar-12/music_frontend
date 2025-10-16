f42b6d31d2e78efd2e341c179d333436ea0839e5
ayush_ahirwar-12 <ayushahirwar04@gmail.com>
2025-10-16 15:43:28 +0530

    path

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import {store} from './store/store.jsx'
+import store from './store/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
